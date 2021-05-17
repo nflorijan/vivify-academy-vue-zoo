5ffd80697b76b42701362f564fef6658671494e2
nflorijan <florijan.nadj@gmail.com>
2021-05-17 21:41:07 +0200

    Zadatak 1, 2

---
+++ src/components/AnimalsList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Animals List</h2>
+    <table>
+      <tr>
+        <th>Vrsta</th>
+        <th>Ime</th>
+        <th>Datum Rodjenja</th>
+      </tr>
+      <tr v-for="(animal, index) in animals" :key="index">
+        <td>{{ animal.vrsta }}</td>
+        <td>{{ animal.ime }}</td>
+        <td>{{ animal.datumRodjenja ? animal.datumRodjenja: 'Nepoznat' }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AnimalsList',
+  data () {
+    return {
+      animals: [
+        {vrsta: 'Pas', ime: 'Zuca', datumRodjenja: new Date(2018, 11, 24)},
+        {vrsta: 'Macka', ime: 'Lunja', datumRodjenja: new Date(2020, 12, 20)},
+        {vrsta: 'Prase', ime: 'Joca', datumRodjenja: new Date(2020, 1, 21)},
+        {vrsta: 'Prase', ime: 'Joca', datumRodjenja: null}
+      ]
+    }
+  }
+}
+</script>
