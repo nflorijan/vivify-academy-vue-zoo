8bdb088014f4c4e01e0ae0bb532682f297d25ab1
nflorijan <florijan.nadj@gmail.com>
2021-05-17 21:57:41 +0200

    Move item to the top

--- src/components/AnimalsList.vue
+++ src/components/AnimalsList.vue
@@ -12,6 +12,7 @@
         <td>{{ animal.ime }}</td>
         <td>{{ animal.datumRodjenja ? animal.datumRodjenja: 'Nepoznat' }}</td>
         <button @click="deleteFromList(index)">Remove</button>
+        <button @click="moveToTop(index)">Move to to top</button>
       </tr>
     </table>
   </div>
@@ -25,14 +26,18 @@ export default {
         {vrsta: 'Pas', ime: 'Zuca', datumRodjenja: new Date(2018, 11, 24)},
         {vrsta: 'Macka', ime: 'Lunja', datumRodjenja: new Date(2020, 12, 20)},
         {vrsta: 'Prase', ime: 'Joca', datumRodjenja: new Date(2020, 1, 21)},
-        {vrsta: 'Prase', ime: 'Joca', datumRodjenja: null}
+        {vrsta: 'Papagaj', ime: 'Papi', datumRodjenja: null}
       ]
     }
   },
   methods: {
     deleteFromList(index) {
       this.animals.splice(index, 1);
-    }
+    },
+    moveToTop(index){
+      const movedItem = this.animals.splice(index, 1);
+      return this.animals.unshift(movedItem[0]);
+    },
   }
 }
 </script>
